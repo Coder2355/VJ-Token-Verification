14323d342ab83160534745ccd489b7bae2e7f180
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2025-01-01 13:51:08 +0530

    Update commands.py

--- commands.py
+++ commands.py
@@ -15,12 +15,17 @@ app = Client("group_verification_bot", api_id=API_ID, api_hash=API_HASH, bot_tok
 
 
 
-@app.on_message(filters.command("start") & filters.private)
+@app.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
-    if len(message.command)>1:
+    if len(message.command) !=2:
         return await message.reply("hi welcome to token verify bot")
     data = message.command[1]
-    if data.split("-", 1)[0] == "verify": # set if or elif it depend on your code
+    try:
+        pre, file_id = data.split('_', 1)
+    except:
+        file_id = data
+        pre = ""
+    if data.split("-", 1)[0] == "verify":
         userid = data.split("-", 2)[1]
         token = data.split("-", 3)[2]
         if str(message.from_user.id) != str(userid):
@@ -40,25 +45,22 @@ async def start(client, message):
                 text="<b>Invalid link or Expired link !</b>",
                 protect_content=True
             )
-
-
-
-@app.on_message(filters.group & filters.chat(GROUP_ID) & filters.private)
-async def enforce_verification(client, message):
-
-    if not await check_verification(client, message.from_user.id) and VERIFY == True:
-        btn = [[
-            InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, f"https://telegram.me/{BOT_USERNAME}?start="))
-        ],[
-            InlineKeyboardButton("How To Open Link & Verify", url=VERIFY_TUTORIAL)
-        ]]
-        await message.reply_text(
-            text="<b>You are not verified !\nKindly verify to continue !</b>",
-            protect_content=True,
-            reply_markup=InlineKeyboardMarkup(btn)
-        )
-        return
-
+    elif data.split("-", 1)[0] == "BATCH":
+        try:
+            if not await check_verification(client, message.from_user.id) and VERIFY_MODE == True:
+                btn = [[
+                    InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, f"https://telegram.me/{username}?start="))
+                ],[
+                    InlineKeyboardButton("How To Open Link & Verify", url=VERIFY_TUTORIAL)
+                ]]
+                await message.reply_text(
+                    text="<b>You are not verified !\nKindly verify to continue !</b>",
+                    protect_content=True,
+                    reply_markup=InlineKeyboardMarkup(btn)
+                )
+                return
+        except Exception as e:
+            return await message.reply_text(f"**Error - {e}**")
 
 
 if __name__ == "__main__":
