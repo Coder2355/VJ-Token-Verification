49fc6c95932119bbbc542b74a082f58c32fb2a80
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2025-01-01 12:01:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 from os import environ
 
-API = environ.get("API", "") # shortlink api
-URL = environ.get("URL", "") # shortlink domain without https://
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
-BOT_USERNAME = environ.get("BOT_USERNAME", "") # bot username without @
+API = environ.get("API", "414b5c709eeaf1b7f73209b5f87ba5bb57069fe4") # shortlink api
+URL = environ.get("URL", "https://instantearn.in") # shortlink domain without https://
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/Anime_Warrior_Tamil") # how to open link 
+BOT_USERNAME = environ.get("BOT_USERNAME", "Hshdgdvdv23bot") # bot username without @
 VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
